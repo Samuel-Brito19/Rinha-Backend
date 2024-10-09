898d1f35e5aaf9c1bc8a6eb870b2412cf41f7459
Samuel-Brito19 <sambriton7@gmail.com>
2024-10-09 17:08:27 -0300

    Function find people

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::{get, post}, Router};
+use axum::{extract::{Path, State}, 
+    http::StatusCode, 
+    response::IntoResponse, 
+    routing::{get, post}, 
+    Router,
+    Json
+};
+use serde::Serialize;
 use uuid::Uuid;
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 use time::{macros::date, Date};
 
+#[derive(Clone,Serialize)]
 struct Person {
     id: Uuid,
     name: String,
@@ -10,6 +18,8 @@ struct Person {
     birth_date: Date,
     stack: Vec<String>
 }
+
+type AppState = Arc<HashMap<Uuid, Person>>;
 #[tokio::main]
 async fn main() {
     let mut people: HashMap<Uuid, Person> = HashMap::new();
@@ -22,12 +32,16 @@ async fn main() {
         stack: vec!["Typescript".to_string(), "Javascript".to_string()]
     };
 
+    println!("{}", person.id);
+
     people.insert(person.id, person);
+    let app_state = Arc::new(people);
     let app = Router::new()
-    .route("/pessoas", get(search_people))
-    .route("/pessoas/:id", get(find_person))
-    .route("/pessoa", post(create_person))
-    .route("/contagem-pessoas", get(count_person));
+    .route("/people", get(search_people))
+    .route("/people/:id", get(find_person))
+    .route("/people", post(create_person))
+    .route("/count-people", get(count_person))
+    .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -37,8 +51,14 @@ async fn search_people() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "Busca de pessoas")
 }
 
-async fn find_person() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "find")
+async fn find_person(State(people): State<AppState>, 
+    Path(person_id): Path<Uuid>) -> impl IntoResponse {
+    //let State(people) = state;
+    match people.get(&person_id) {
+        Some(person) => Ok(Json(person.clone())),
+        None => Err(StatusCode::NOT_FOUND),
+    }
+
 }
 
 async fn create_person() -> impl IntoResponse {
